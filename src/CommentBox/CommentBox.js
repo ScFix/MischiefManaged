c57d97cc20ddddf5afd391dffe82beea97d3004c
Matthew Wilkinson <Wilkinson.M@outlook.com>
2016-10-11 21:53:05 -0400

    This is the inital start of the project. This is the basic react module using the visual studio code as my editor of choice

---
+++ src/CommentBox/CommentBox.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './CommentBox.css';
+
+class CommentBox extends Component{
+    render (){
+      return (
+        <div className="commentBox">
+          Hello, World! I am a CommentBox.
+        </div>
+      );
+    }
+}
+
+export default CommentBox;
