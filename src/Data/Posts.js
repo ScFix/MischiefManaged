0798200dc0749ce4b0c6ff1f63ed964621ec0100
Matthew Wilkinson <Wilkinson.M@outlook.com>
2016-10-28 14:22:58 -0400

    Converted the posts.js to imgur platform.
    Will see if this platfor will support my meager needs.

--- src/Data/Posts.js
+++ src/Data/Posts.js
@@ -3,73 +3,73 @@ const Posts = {
         {
             code: 11,
             title: 'Just arriving',
-            imageUrl: 'http://s19.postimg.org/gktl7ko03/IMG_4545.jpg',
+            imageUrl: 'http://i.imgur.com/KISZkRp.jpg',
             likes: 0
         },
         {
             code: 12,
             title: 'Clearwater here we come!',
-            imageUrl: 'http://s19.postimg.org/w50yy3y4j/IMG_4542.jpg',
+            imageUrl: 'http://i.imgur.com/m3iL7rf.jpg',
             likes: 0
         },
         {
             code: 13,
             title: 'Relaxing in the room.',
-            imageUrl: 'https://s19.postimg.org/z16l5pksj/IMG_4560.jpg',
+            imageUrl: 'http://i.imgur.com/gJBHWMa.jpg',
             likes: 0
         },
         {
             code: 13,
             title: 'Looking good',
-            imageUrl: 'https://s19.postimg.org/viul9bjwj/IMG_4561.jpg',
-            likes: 0
-        },
-        {
-            code: 13,
-            title: 'Mmmm mmmm good',
-            imageUrl: 'https://s19.postimg.org/jv54sik5f/IMG_4565.jpg',
+            imageUrl: 'http://i.imgur.com/npwm6a2.jpg?1',
             likes: 0
         },
         {
             code: 13,
             title: 'Gotta pose',
-            imageUrl: 'https://s19.postimg.org/hf3beo22r/IMG_4567.jpg',
+            imageUrl: 'http://i.imgur.com/v0aXSlT.jpg',
+            likes: 0
+        },
+        {
+            code: 13,
+            title: 'Mmmm mmmm good',
+            imageUrl: 'http://i.imgur.com/XWoTKJB.jpg',
             likes: 0
         },
         {
             code: 13,
             title: 'Dem teeth!',
-            imageUrl: 'https://s19.postimg.org/51qh7rceb/IMG_4569.jpg',
+            imageUrl: 'http://i.imgur.com/weNA0Yd.jpg',
             likes: 0
         },
         {
             code: 13,
             title: 'Sickening PDA',
-            imageUrl: 'https://s19.postimg.org/9owj9izr7/IMG_4573.jpg',
+            imageUrl: 'http://i.imgur.com/mbqALOt.jpg',
             likes: 0
         },
         {
             code: 13,
             title: 'Perfect end for a Perfect day',
-            imageUrl: 'https://s19.postimg.org/dzb7544ub/IMG_4582.jpg',
+            imageUrl: 'http://i.imgur.com/YPczxEK.jpg',
             likes: 0
         },
         {
             code: 13,
-            title: 'Ooo look a fish',
-            imageUrl: 'https://s19.postimg.org/lgkekbudf/IMG_4584.jpg',
+            title: 'Ooo look flowers',
+            imageUrl: 'http://i.imgur.com/qlmuCH2.jpg',
             likes: 0
         },
         {
             code: 13,
             title: 'I am so cool',
-            imageUrl: 'https://s19.postimg.org/tal05q26b/IMG_4586.jpg',
+            imageUrl: 'http://i.imgur.com/n2liovE.jpg',
             likes: 0
         },
         {
             code: 13,
             title: 'Need to move here',
-            imageUrl: 'https://s19.postimg.org/nax922hdv/IMG_4588.jpg',
+            imageUrl: 'http://i.imgur.com/YpYq8Vj.jpg',
             likes: 0
         },
     ],
